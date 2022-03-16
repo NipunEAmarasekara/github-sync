2290362cc12c2da14e9f93dc2c96daa373686392
NipunEAmarasekara <namarasekara@typefi.com>
2022-03-16 11:03:42 +0530

    changes added

--- backup.js
+++ backup.js
@@ -120,7 +120,7 @@ async function backupProcess() {
             });
 
             codecommit.getRepository({ repositoryName: `${username}_${repo}` }, function (err, data) {
-                if (!(data.repositoryMetadata.defaultBranch === 'main' || data.repositoryMetadata.defaultBranch === 'master')) {
+                if (data.repositoryMetadata.defaultBranch !== repository.default_branch) {
                     try {
                         codecommit.updateDefaultBranch({ defaultBranchName: repository.default_branch, repositoryName: `${username}_${repo}` }, function (err, data) {
                             if (err === null)
