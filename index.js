c039684fa2362438297ca8336bc5be16aabf275c
NipunEAmarasekara <namarasekara@typefi.com>
2022-03-18 14:28:35 +0530

    internal schedular added

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const backup = require('./backup');
 const express = require('express')
+const cron = require('node-cron');
 const app = express();
 const port = 8080
 
 
 const server = app.listen(port, async () => {
   console.log(`Automatic github backup process is started`);
-  await backup.init();
-  
-  process.on('SIGTERM', () => {
-    server.close();
+  cron.schedule('* * * * *', async function () {
+    await backup.init();
   });
 
-  process.kill(process.pid, 'SIGTERM')
+  // process.on('SIGTERM', () => {
+  //   server.close();
+  // });
+
+  // process.kill(process.pid, 'SIGTERM')
 });
