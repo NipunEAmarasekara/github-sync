ea730cba4f6dd8c5842cc79cf81424683a534b5d
NipunEAmarasekara <namarasekara@typefi.com>
2022-03-21 07:56:16 +0530

    project configured to backup localOnly

--- backup.js
+++ backup.js
@@ -91,19 +91,19 @@ async function backupProcess() {
             let repo = repository.name;
 
             //Check if the repository exists on codecommit.Create a repository if it doesn't exists.
-            codecommit.getRepository({ repositoryName: `${username}_${repo}` }, function (err, data) {
-                if (err) {
-                    if (err.code === 'RepositoryDoesNotExistException') {
-                        if (repository.description) {
-                            if (repository.description != "")
-                                child_process.execSync(`aws codecommit create-repository --repository-name ${username}_${repo} --repository-description "${(repository.description) ? repository.description : ''}"`);
-                            else
-                                child_process.execSync(`aws codecommit create-repository --repository-name ${username}_${repo}`);
-                        } else
-                            child_process.execSync(`aws codecommit create-repository --repository-name ${username}_${repo}`);
-                    }
-                }
-            });
+            // codecommit.getRepository({ repositoryName: `${username}_${repo}` }, function (err, data) {
+            //     if (err) {
+            //         if (err.code === 'RepositoryDoesNotExistException') {
+            //             if (repository.description) {
+            //                 if (repository.description != "")
+            //                     child_process.execSync(`aws codecommit create-repository --repository-name ${username}_${repo} --repository-description "${(repository.description) ? repository.description : ''}"`);
+            //                 else
+            //                     child_process.execSync(`aws codecommit create-repository --repository-name ${username}_${repo}`);
+            //             } else
+            //                 child_process.execSync(`aws codecommit create-repository --repository-name ${username}_${repo}`);
+            //         }
+            //     }
+            // });
 
             //Get repository branches list from the github
             const branches = (await octokit.rest.repos.listBranches({ owner: repository.owner.login, repo: repository.name })).data;
@@ -113,42 +113,42 @@ async function backupProcess() {
                 if (!fs.existsSync(`${config.LOCAL_BACKUP_PATH}/repos/${username}/${repo}`)) {
                     child_process.execSync(`git clone https://${username}:${config.GITHUB_ACCESS_TOKEN}@github.com/${username}/${repo}.git ${config.LOCAL_BACKUP_PATH}/repos/${username}/${repo}`);
                     child_process.execSync(`cd ${config.LOCAL_BACKUP_PATH}/repos/${repository.owner.login}/${repository.name} && git fetch && git checkout ${branch.name} && git pull origin ${branch.name}`);
-                    child_process.execSync(`cd ${config.LOCAL_BACKUP_PATH}/repos/${repository.owner.login}/${repository.name} && git push ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/${repository.owner.login}_${repository.name} ${branch.name}`);
+                    //child_process.execSync(`cd ${config.LOCAL_BACKUP_PATH}/repos/${repository.owner.login}/${repository.name} && git push ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/${repository.owner.login}_${repository.name} ${branch.name}`);
                     console.log(`${repo} Repository ${branch.name} Branch Cloned\n`);
                 } else {
                     child_process.execSync(`cd ${config.LOCAL_BACKUP_PATH}/repos/${repository.owner.login}/${repository.name} && git fetch && git checkout ${branch.name} && git pull origin ${branch.name}`);
-                    child_process.execSync(`cd ${config.LOCAL_BACKUP_PATH}/repos/${repository.owner.login}/${repository.name} && git push ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/${repository.owner.login}_${repository.name} ${branch.name}`);
+                    //child_process.execSync(`cd ${config.LOCAL_BACKUP_PATH}/repos/${repository.owner.login}/${repository.name} && git push ssh://git-codecommit.us-east-1.amazonaws.com/v1/repos/${repository.owner.login}_${repository.name} ${branch.name}`);
                     console.log(`${repository.name} Repository ${branch.name} Branch Updated\n`);
                 }
             });
 
             //If the github repository default branch is not the default branch in codecommit. set it to the original default branch.
-            codecommit.getRepository({ repositoryName: `${username}_${repo}` }, function (err, data) {
-                if (data.repositoryMetadata.defaultBranch !== repository.default_branch) {
-                    try {
-                        codecommit.updateDefaultBranch({ defaultBranchName: repository.default_branch, repositoryName: `${username}_${repo}` }, function (err, data) {
-                            if (err === null)
-                                console.log(`Default branch set to ${repository.default_branch} in ${username}_${repo}`);
-                        });
-                    } catch (e) {
-                        console.log(e);
-                    }
-                }
-            });
+            // codecommit.getRepository({ repositoryName: `${username}_${repo}` }, function (err, data) {
+            //     if (data.repositoryMetadata.defaultBranch !== repository.default_branch) {
+            //         try {
+            //             codecommit.updateDefaultBranch({ defaultBranchName: repository.default_branch, repositoryName: `${username}_${repo}` }, function (err, data) {
+            //                 if (err === null)
+            //                     console.log(`Default branch set to ${repository.default_branch} in ${username}_${repo}`);
+            //             });
+            //         } catch (e) {
+            //             console.log(e);
+            //         }
+            //     }
+            // });
 
             //Remove deleted branches
-            codecommit.listBranches({ repositoryName: `${username}_${repo}` }, function (err, data) {
-                data.branches.forEach(cb => {
-                    if (!(branches.filter(b => b.name === cb).length > 0)) {
-                        codecommit.deleteBranch({ branchName: cb, repositoryName: `${username}_${repo}` }, function (err, data) {
-                            if(err === null)
-                                console.log(`${cb} branch removed from codecommit.`);
-                            else
-                                console.log(err);
-                        });
-                    }
-                });
-            });
+            // codecommit.listBranches({ repositoryName: `${username}_${repo}` }, function (err, data) {
+            //     data.branches.forEach(cb => {
+            //         if (!(branches.filter(b => b.name === cb).length > 0)) {
+            //             codecommit.deleteBranch({ branchName: cb, repositoryName: `${username}_${repo}` }, function (err, data) {
+            //                 if(err === null)
+            //                     console.log(`${cb} branch removed from codecommit.`);
+            //                 else
+            //                     console.log(err);
+            //             });
+            //         }
+            //     });
+            // });
 
             count++;
         });
