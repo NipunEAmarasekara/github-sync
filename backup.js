4b4cddf84c9a4a427703b1d28ac2f67934978ebe
NipunEAmarasekara <namarasekara@typefi.com>
2022-03-23 14:28:56 +0530

    s3 test

--- backup.js
+++ backup.js
@@ -177,52 +177,42 @@ async function backupProcess() {
     }
 }
 
-// async function copyReposToS3(repo) {
-//     try {
-//         const uploader = Promise.promisify(s3.upload.bind(s3));
-//         const passThroughStream = new stream.PassThrough();
-//         const arhiveURL =
-//             "https://api.github.com/repos/" +
-//             repo.full_name +
-//             "/tarball/master?access_token=" +
-//             config.GITHUB_ACCESS_TOKEN;
-//         const requestOptions = {
-//             url: arhiveURL,
-//             headers: {
-//                 "User-Agent": "nodejs"
-//             }
-//         };
-
-//         request(requestOptions).pipe(passThroughStream);
-//         const bucketName = config.AWS_S3_BUCKET_NAME;
-//         const objectName = repo.full_name + ".tar.gz";
-//         const params = {
-//             Bucket: bucketName,
-//             Key: objectName,
-//             Body: passThroughStream,
-//             //StorageClass: options.s3StorageClass || "STANDARD",
-//             StorageClass: "STANDARD",
-//             ServerSideEncryption: "AES256"
-//         };
-
-//         return uploader(params).then(result => {
-//             console.log(`[✓] ${repo.full_name} Repository synced to s3.\n`)
-//         });
-//     } catch (e) {
-//         console.log(e);
-//     }
-// }
-
 async function copyReposToS3(repo) {
     try {
-        child_process.execSync(`zip ${config.LOCAL_BACKUP_PATH}/repos/${repo.owner.login}/${repo.name}.zip ${config.LOCAL_BACKUP_PATH}/repos/${repo.owner.login}/${repo.name}`, options);
-        const command = `aws s3 sync ${config.LOCAL_BACKUP_PATH}/repos/${repo.owner.login} s3://${config.AWS_S3_BUCKET_NAME}`;
-        child_process.execSync(command, options);
-        console.log(`[✓] ${repo.full_name} Repository synced to s3.\n`);
+        const uploader = Promise.promisify(s3.upload.bind(s3));
+        const passThroughStream = new stream.PassThrough();
+        const arhiveURL =
+            "https://api.github.com/repos/" +
+            repo.full_name +
+            "/zipball/*?access_token=" +
+            config.GITHUB_ACCESS_TOKEN;
+        const requestOptions = {
+            url: arhiveURL,
+            headers: {
+                "User-Agent": "nodejs"
+            }
+        };
+
+        request(requestOptions).pipe(passThroughStream);
+        const bucketName = config.AWS_S3_BUCKET_NAME;
+        const objectName = repo.full_name + ".zip";
+        const params = {
+            Bucket: bucketName,
+            Key: objectName,
+            Body: passThroughStream,
+            //StorageClass: options.s3StorageClass || "STANDARD",
+            StorageClass: "STANDARD",
+            ServerSideEncryption: "AES256"
+        };
+
+        return uploader(params).then(result => {
+            console.log(`[✓] ${repo.full_name} Repository synced to s3.\n`)
+        });
     } catch (e) {
         console.log(e);
     }
 }
+
 
 module.exports.init = async (m) => {
     mode = m;
