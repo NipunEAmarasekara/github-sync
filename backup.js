94c91e70daedae234d917af386c0c9a41f1c7e7e
NipunEAmarasekara <namarasekara@typefi.com>
2022-03-25 13:46:40 +0530

    changes added

--- backup.js
+++ backup.js
@@ -242,7 +242,7 @@ async function localToS3() {
                     };
 
                     try {
-                        await s3.upload(params, { partSize: 10 * 1024 * 1024, queueSize: 5 }).promise();
+                        await s3.upload(params, { partSize: 100 * 1024 * 1024, queueSize: 5 }).promise();
                         //child_process.execSync(`rm ${config.LOCAL_BACKUP_PATH}/repos/${repo.full_name}.zip`, options);
                         console.log('upload OK', `${config.LOCAL_BACKUP_PATH}/repos/${repo.full_name}.zip`);
                     } catch (error) {
